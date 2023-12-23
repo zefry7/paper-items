bc33f497a197832a73e104a4707a7e1835d68430
nikita petrov <petrov.nikita702@mail.ru>
2023-12-23 08:07:23 +0300

    4-ый блок

--- src/Material/material.js
+++ src/Material/material.js
@@ -3,6 +3,10 @@ import i2 from "./img/item-2.webp"
 import i3 from "./img/item-3.webp"
 import i4 from "./img/item-4.webp"
 
+import ic1 from "./img/icon-1.svg"
+import ic2 from "./img/icon-2.svg"
+import ic3 from "./img/icon-3.svg"
+import ic4 from "./img/icon-4.svg"
 
 function Material() {
     const mass = [
@@ -32,19 +36,32 @@ function Material() {
         },
     ]
 
+    const icons = [ic1, ic2, ic3, ic4]
+
     return (
         <seciton class="material">
-            {mass.map(value => (
-                <div class="material__item">
-                    <div class="material__img">
-                        <img src={value.img} alt="Типы изделий" />
+            <div class="material__content">
+                {mass.map(value => (
+                    <div class="material__item">
+                        <div class="material__img">
+                            <img src={value.img} alt="Типы изделий" />
+                        </div>
+                        <div className="material__text">
+                            <h5 class="material__name">{value.name}</h5>
+                            <p class="material__type">{value.count}</p>
+                            <p class="material__description">{value.desc}</p>
+                            <p class="material__more">Подробнее</p>
+                        </div>
                     </div>
-                    <h5 class="material__name">{value.name}</h5>
-                    <p class="material__type">{value.count}</p>
-                    <p class="material__description">{value.desc}</p>
-                    <p class="material__more">Подробнее</p>
-                </div>
-            ))}
+                ))}
+            </div>
+            <div class="material__icons">
+                {icons.map(value => (
+                    <div class="material__icons-item">
+                        <img src={value} alt="Фирма" />
+                    </div>
+                ))}
+            </div>
         </seciton>
     );
 }
